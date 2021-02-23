f8f74b17448bba9a7a85dfd450534c2b0322c4a9
claude-hub <314705487@qq.com>
2021-02-23 23:01:10 +0800

    feat: init webpack cli

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React, { PureComponent } from "react";
+import ReactDom from "react-dom";
+
+class App extends PureComponent {
+  render() {
+    return <div>Hello React</div>;
+  }
+}
+
+ReactDom.render(<App />, document.getElementById("app"));
